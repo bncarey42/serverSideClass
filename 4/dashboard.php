bb1eb33718df48d7f2e95b5e17adcd8e99e93952
BenCarey <bncarey42@gmail.com>
2017-10-30 12:23:02 -0500

    updated dashboard

--- 4/dashboard.php
+++ 4/dashboard.php
@@ -1,14 +1,20 @@
 <?php include("assets/includes/header.php"); ?>
+
   <fieldset>
     <table>
     <tr>
       <th>STUDENT NAME</th><th>COURSE NUMBER</th><th>VIEW</th><th>DELETE</th>
     </tr>
-    foreach($surveys as $survey){
       <tr>
-        <th></th>
-      </tr>
+    <?php
+        $rowNum = 0;
+    foreach($surveys as $survey){
+      ?>
+        <th class=<?php echo ($rumNum%2==0) ? "even" : "odd" ?>></th>
     }
+        <th><a href="delete.php">Delete</a></th>
+        </tr>
     </table>
   </fieldset>
+
 <?php include("assets/includes/footer.php");?>

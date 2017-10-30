e12855082825fba356b2dde0df4a59615197fa12
bncarey42 <bncarey42@gmail.com>
2017-10-30 01:10:22 -0500

    updated form vlaidation

--- 4/assets/includes/forms/student.php
+++ 4/assets/includes/forms/student.php
@@ -1,14 +1,16 @@
 <form action="survey.php" method="post" id="studentInputForm">
-  <input type="text" name="fname" value="First Name" required />
-  <input type="text" name="lname" value="Last Name" required />
+  <label for="fname">First Name</label>
+  <label for="lname">Last Name</label>
+  <input type="text" name="fname" value="<?php check_input($_POST['fname'] ?>)" required />
+  <input type="text" name="lname" value="<?php check_input($_POST['fname'] ?>" required />
+<label for="gender">Please select your gender</label>
+<label for="cohort">Please select your current cohort</label>
   <select name="gender" form="studentInputForm" required>
-    <option>Please select your gender</option>
     <option value="F">Female</option>
     <option value="M">Male</option>
     <option value="$">Other/Pref. not Disclose</option>
   </select>
   <select name="cohort" form="studentInputForm" required>
-    <option>Please select your current cohort</option>
     <option value="F">Freshman</option>
     <option value="S">Sophmore</option>
     <option value="J">Junior</option>
@@ -16,10 +18,14 @@
     <option value="$">Senior+/Grad/Other</option>
   </select>
   </br>
-  <input class="dpt" type="text" name="courseDpt" value="Department code(three letters)" required/>
-  <input type="text" name="courseNum" value="Course number(four numbers)" required/>
+  <lable for="dpt">Department code(three letters)</lable>
+  <lable for="courseNum">Course number(four numbers)</lable>
+  <input class="dpt" type="text" name="courseDpt" value="" required/>
+  <input type="number" name="courseNum" value="" required/>
   <div class="buttons">
     <input type="submit" value="Begin Survey" />
     <input type="reset" />
   </div>
 </form>
+
+function check_input($input)

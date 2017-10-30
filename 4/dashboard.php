7d1de3e8de81756c4c04187505eb4d945ede8abe
BenCarey <bncarey42@gmail.com>
2017-10-30 09:29:11 -0500

    Update dashboard.php

--- 4/dashboard.php
+++ 4/dashboard.php
@@ -1,5 +1,14 @@
 <?php include("assets/includes/header.php"); ?>
-<div class="content">
-
-</div>
+  <fieldset>
+    <table>
+    <tr>
+      <th>STUDENT NAME</th><th>COURSE NUMBER</th><th>VIEW</th><th>DELETE</th>
+    </tr>
+    foreach($surveys as $survey){
+      <tr>
+        <th></th>
+      </tr>
+    }
+    </table>
+  </fieldset>
 <?php include("assets/includes/footer.php");?>

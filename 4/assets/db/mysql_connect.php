b6e619dee813e77f43a06cfd21544d7633a9968b
bncarey42 <bncarey42@gmail.com>
2017-10-30 01:46:21 -0500

    updates

--- 4/assets/db/mysql_connect.php
+++ 4/assets/db/mysql_connect.php
@@ -2,9 +2,9 @@
 $host="192.185.225.1";
 $port=3306;
 $socket="";
-$user="cjohnson_qu5773o";
+$user="cjohnson_qu5773o@localhost";
 $password="12856076bc";
-$dbname="cjohnson_qu5773oo";
+$dbname="";
 
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
 	or die ('Could not connect to the database server' . mysqli_connect_error());
--- 4/assets/includes/forms/student.php
+++ 4/assets/includes/forms/student.php
@@ -1,31 +1,64 @@
+<div class="form">
 <form action="survey.php" method="post" id="studentInputForm">
-  <label for="fname">First Name</label>
-  <label for="lname">Last Name</label>
-  <input type="text" name="fname" value="<?php check_input($_POST['fname'] ?>)" required />
-  <input type="text" name="lname" value="<?php check_input($_POST['fname'] ?>" required />
-<label for="gender">Please select your gender</label>
-<label for="cohort">Please select your current cohort</label>
-  <select name="gender" form="studentInputForm" required>
-    <option value="F">Female</option>
-    <option value="M">Male</option>
-    <option value="$">Other/Pref. not Disclose</option>
-  </select>
-  <select name="cohort" form="studentInputForm" required>
-    <option value="F">Freshman</option>
-    <option value="S">Sophmore</option>
-    <option value="J">Junior</option>
-    <option value="G">Senior</option>
-    <option value="$">Senior+/Grad/Other</option>
-  </select>
-  </br>
-  <lable for="dpt">Department code(three letters)</lable>
-  <lable for="courseNum">Course number(four numbers)</lable>
-  <input class="dpt" type="text" name="courseDpt" value="" required/>
-  <input type="number" name="courseNum" value="" required/>
+  <table>
+    <tr>
+        <th>  <label for="fname">First Name </label> </th>
+        <th><label for="lname">Last Name</label></th>
+    </tr>
+    <tr>
+        <th><input type="text" name="fname" value="<?php check_input($_POST['fname']) ?>" /></th>
+        <th><input type="text" name="lname" value="<?php check_input($_POST['lname']) ?>" required /></th>
+    </tr>
+    <tr>
+      <th><label for="gender">Please select your gender</label></th>
+      <th><label for="cohort">Please select your current cohort</label></th>
+    </tr>
+    <tr>
+      <th>
+        <select name="gender" form="studentInputForm" required>
+          <option></option>
+          <option value="F">Female</option>
+          <option value="M">Male</option>
+          <option value="$">Other/Pref. not Disclose</option>
+        </select>
+      </th>
+        <th>
+          <select name="cohort" form="studentInputForm" required>
+            <option></option>
+            <option value="F">Freshman</option>
+            <option value="S">Sophmore</option>
+            <option value="J">Junior</option>
+            <option value="G">Senior</option>
+            <option value="$">Senior+/Grad/Other</option>
+          </select>
+      </th>
+    </tr>
+    <tr>
+      <th><lable for="dpt">Department code(four(4) letters)</lable></th>
+        <th><lable for="courseNum">Course number(up to four(4) numbers)</lable></th>
+    </tr>
+    <tr>
+      <th><input class="dpt" type="text" name="courseDpt" value="" required maxlength="4"/></th>
+        <th><input type="number" name="courseNum" value="" required maxlength="4"/></th>
+    </tr>
+  </table>
   <div class="buttons">
     <input type="submit" value="Begin Survey" />
     <input type="reset" />
   </div>
 </form>
+</div>
 
-function check_input($input)
+<?php
+function check_input($input){
+  switch ($input) {
+    case isset($input):
+      return $input;
+      break;
+
+    default:
+      # code...
+      break;
+  }
+}
+?>

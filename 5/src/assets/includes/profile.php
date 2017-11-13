771e771a46a2f8e4cafbddc8c6d46e92acb1a598
BenCarey <bncarey42@gmail.com>
2017-11-13 10:49:19 -0600

    Update profile.php

--- 5/src/assets/includes/profile.php
+++ 5/src/assets/includes/profile.php
@@ -1,7 +1,32 @@
 <?php
-$newMadLib = isset($_POST('saveMadLib');
-if(isset($newMadLib) && $newMadLib=="Save MadLib"){
-  //new_entry()
-  echo "lets go!";
-}
- ?>
+  $newMadLib = isset($_POST('saveMadLib');
+  if(isset($newMadLib) && $newMadLib=="Save MadLib"){
+    //new_entry()
+    echo "lets go!";
+  }
+
+?>
+<fieldset class="surveyResults">
+  <table>
+    <form action="assets/forms/updateMadLib.php" method="post">
+    <?php
+      $rowNum = 0;
+      $r = getUserMadLibs();
+        if($r){
+        while ($row = mysqli_fetch_array($r, MYSQLI_BOTH)) {
+          $rowClass=(($rowNum%2==0) ? "even" : "odd");
+          echo" <tr class=\"$rowClass\">
+                  <th>
+                    <input type=\"radio\" name=\"mlid\" value=\"$row['mlid']\" hidden \>
+                    <input type=\"radio\" />
+                  </th>
+                </tr>
+          ";
+        }
+
+      ?>
+      <input type="submit" name="updateMadLib" value="View" />
+      <input type="submit" name="updateMadLib" value="Delete" />
+    </form>
+  </table>
+</feildset>

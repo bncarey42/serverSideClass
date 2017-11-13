848f790b2acda458deea8c3f97fa253a5460619f
BenCarey <bncarey42@gmail.com>
2017-11-13 10:53:59 -0600

    Update profile.php

--- 5/src/assets/includes/profile.php
+++ 5/src/assets/includes/profile.php
@@ -13,17 +13,21 @@
       $rowNum = 0;
       $r = getUserMadLibs();
         if($r){
-        while ($row = mysqli_fetch_array($r, MYSQLI_BOTH)) {
-          $rowClass=(($rowNum%2==0) ? "even" : "odd");
-          echo" <tr class=\"$rowClass\">
-                  <th>
-                    <input type=\"radio\" name=\"mlid\" value=\"$row['mlid']\" hidden \>
-                    <input type=\"radio\" />
-                  </th>
-                </tr>
-          ";
-        }
-
+          while ($row = mysqli_fetch_array($r, MYSQLI_BOTH)) {
+            $rowClass=(($rowNum%2==0) ? "even" : "odd");
+            echo" <tr class=\"$rowClass\">
+                    <th>
+                      <input type=\"radio\" name=\"mlid\" value=\"$row['mlid']\" hidden \>
+                      <input type=\"radio\" />
+                    </th>
+                    <th>Jack and the $row['']</th>
+                  </tr>
+            ";
+          }
+        } else {
+          echo "<tr>
+          <th>No MadLibs found for this user</th>
+          </tr>";
       ?>
       <input type="submit" name="updateMadLib" value="View" />
       <input type="submit" name="updateMadLib" value="Delete" />

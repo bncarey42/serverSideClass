b01b5aad9b9784341422cd741096096e499067dc
BenCarey <bncarey42@gmail.com>
2017-11-13 12:17:10 -0600

    Update user.php

--- 5/src/user.php
+++ 5/src/user.php
@@ -1,5 +1,16 @@
 <?php include('assets/includes/header.php);
-if($logedOn){
+if(isset($_POST('createUser')) && $_POST('createUser') == "Create User") {
+  $email=$_POST('email');
+  $passwd=$_POST('passwd');
+  $firstName=$_POST('fName');
+  $lastName=$_POST('lName');
+
+  if(createUser($email, $passwd, $firstName, $lastName)){
+    $currentUID=getUIDByEmail($email);
+  }
+}
+
+if($loggedOn){
   include('assets/forms/profile.php');
 } else {
   include('assets/forms/createUser.php');

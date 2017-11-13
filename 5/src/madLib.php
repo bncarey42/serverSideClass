cab55510189bd26a5b1f6e79ccdd264067318f8c
BenCarey <bncarey42@gmail.com>
2017-11-13 08:29:58 -0600

    Update madLib.php
    move madLib form to assets/includes

--- 5/src/madLib.php
+++ 5/src/madLib.php
@@ -1,16 +1,3 @@
-<?php include('assets/includes/header.php'); ?>
-  <form action="assets/includes/displayMadLib.php" method="post">
-      <input type="text"  neam="plural_noun_one" value="plural noun" />
-      <input type="text"  neam="plural_noun_two" value="plural noun" />
-      <input type="text"  neam="adjective_one" value="adjective" />
-      <input type="text"  neam="noun_one" value="noun" />
-      <input type="text"  neam="adjective_two" value="adjective" />
-      <input type="text"  neam="adjective_three" value="adjective" />
-      <input type="text"  neam="verb" value="verb"/>
-      <input type="text"  neam="body_part" value="body part"/>
-      <input type="text"  neam="adjective_four" value="adjective" />
-      <input type="text" neam="number" value="number" />
-      <input type="text"  neam="noun_two" value="noun" />
-      <input type="submit"/>
-  </form>
-<?php include('assets/includes/footer.php'); ?>
+<?php include('assets/includes/header.php');
+include('assets/includes/madLib.php');
+include('assets/includes/footer.php'); ?>

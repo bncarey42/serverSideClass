852308c547283d0ba2352297371a1adb03bb256a
BenCarey <bncarey42@gmail.com>
2017-11-13 09:39:52 -0600

    Update logIn.php

--- 5/src/logIn.php
+++ 5/src/logIn.php
@@ -1,7 +1,7 @@
 <?php include('assets/includes/header.php);
 if($logedOn){
-  include('../forms/profile.php');
+  include('assets/forms/profile.php');
 } else {
-  include('../forms/createUser.php');
+  include('assets/forms/createUser.php');
 }
 include('assets/includes/footer.php'); ?>

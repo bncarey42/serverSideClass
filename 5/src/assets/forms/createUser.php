f53916c13723d9c4d700a8cffeed4ebf82384a3a
BenCarey <bncarey42@gmail.com>
2017-11-13 11:58:57 -0600

    Update createUser.php

--- 5/src/assets/forms/createUser.php
+++ 5/src/assets/forms/createUser.php
@@ -3,7 +3,7 @@ $email = $_POST('email');
 ?>
 <form action="user.php" method="post">
   <input type="text" name="fName"/>
-  <input type="text" name="lname"/>
+  <input type="text" name="lName"/>
   <input type="email" name="email" value="<?php echo "$email"; ?>" />
   <input type="password" name="passwd" />
   <input type="submit" value="Create User" name="createUser" />

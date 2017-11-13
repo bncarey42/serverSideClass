9e8bc27ee04fe50dc70a927d6b107a5f2cc2f105
BenCarey <bncarey42@gmail.com>
2017-11-13 09:35:57 -0600

    Update index.php

--- 5/src/index.php
+++ 5/src/index.php
@@ -1,9 +1,5 @@
 <?php
 include('assets/includes/header.php');
-if($loggedOn){
-  include('assets/includes/profile.php');
-} else {
   include('assets/includes/login.html');
-}
 include('assets/includes/footer.php');
 ?>

5537d6dec1c6f0689c3c101c72a50771df3c40d7
BenCarey <bncarey42@gmail.com>
2017-11-13 12:35:36 -0600

    Update profile.php

--- 5/src/assets/includes/profile.php
+++ 5/src/assets/includes/profile.php
@@ -20,7 +20,47 @@
                       <input type=\"radio\" name=\"mlid\" value=\"$row['mlid']\" hidden \>
                       <input type=\"radio\" />
                     </th>
-                    <th>Jack and the $row['']</th>
+                    <th>Jack and the $row['<?php
+  $newMadLib = isset($_POST('saveMadLib');
+  if(isset($newMadLib) && $newMadLib=="Save MadLib"){
+    //new_entry()
+    echo "lets go!";
+  }
+  $userName=getUserNameByUID($uid);
+
+?>
+<h3>
+  <?php  echo "Hello $userName"?>
+</h3>
+<fieldset class="surveyResults">
+  <table>
+    <form action="assets/forms/updateMadLib.php" method="post">
+    <?php
+      $rowNum = 0;
+      $r = getUserMadLibs();
+        if($r){
+          while ($row = mysqli_fetch_array($r, MYSQLI_BOTH)) {
+            $rowClass=(($rowNum%2==0) ? "even" : "odd");
+            echo"
+                  <tr class=\"$rowClass\">
+                    <th>
+                      <input type=\"radio\" name=\"mlid\" value=\"$row['mlid']\" hidden \>
+                      <input type=\"radio\" />
+                    </th>
+                    <th>Jack and the $row['noun_one']</th>
+                  </tr>
+            ";
+          }
+        } else {
+          echo "<tr>
+          <th>No MadLibs found for this user</th>
+          </tr>";
+      ?>
+      <input type="submit" name="updateMadLib" value="View MadLib" />
+      <input type="submit" name="updateMadLib" value="Delete" />
+    </form>
+  </table>
+</feildset>']</th>
                   </tr>
             ";
           }

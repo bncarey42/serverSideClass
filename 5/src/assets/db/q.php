f57cff015a4d771f9409368b3a3ad9c1ecad6e50
BenCarey <bncarey42@gmail.com>
2017-11-13 12:16:12 -0600

    Update q.php

--- 5/src/assets/db/q.php
+++ 5/src/assets/db/q.php
@@ -5,20 +5,31 @@ function userExists($email) {
   $findUserByEmail = "SELECT USR_EMAIL FROM cjohnson_qu5773oo.User WHERE USR_EMAIL=\'$email\'";
 
   $r = @mysqli_query($dbc, $findUserByEmail);
-  $exists = $r
-  return $exists
+  $exists = $r;
+  return $exists;
 }
 
 function isCorectPassword($email, $passwd){
-$correct = false;
+  $correct = false;
   require ('mysqli_connect.php');
   $selectPasswordForUserByEmail = "SELECT USR_PASSWORD as password FROM cjohnson_qu5773oo.User WHERE USR_EMAIL=\'$email\'";
   $r = @mysqli_query($dbc, $selectPasswordForUserByEmail);
   if($r){
     while($row = mysqli_fetch_array($r, MYSQLI_BOTH)){
-      if($passwd = row['password']){
+      if($passwd = $row['password']){
         $loggedOn=true;
       }
+    }
+  }
+}
+
+function getUIDbyEmail($email){
+  require ('mysqli_connect.php');
+  $selectUIDByEmail = "SELECT USR_ID as id FROM cjohnson_qu5773oo.User WHERE USR_EMAIL=\'$email\'";
+  $r = @mysqli_query($dbc, $selectUIDByEmail);
+  if($r){
+    while($row=mysqli_fetch_array($r, MYSQLI_BOTH)){
+      return $row['id'];
     }
   }
 }
@@ -37,6 +48,10 @@ fuction getMadLib($mlid){
 function createUser($email, $passwd, $fname, $lname){
   $createUser="INSERT into cjohnson_qu5773oo.User(USR_EMAIL, USR_PASSWORD, USR_FIRST_NAME, USR_LAST_NAME) VALUES ($email, $passwd, $fname, $lname)";
   require ('mysqli_connect.php');
-    
-}
+  $r = @mysqli_query($dbc, $insertStudent);
+  
+  if($r){
+    $loggedOn = true; 
+  }
+  return $r;
 ?>

5e2b6f0e596afdac1f448cfae0ada53c6c496e47
BenCarey <bncarey42@gmail.com>
2017-11-28 15:06:30 -0600

    Update index.php

--- 5/src/index.php
+++ 5/src/index.php
@@ -1,5 +1,14 @@
-<?php
-include('assets/includes/header.php');
-include('assets/forms/login.html');
-include('assets/includes/footer.php');
+<?php session_start();
+	include('assets/includes/header.html');
+	if(isset($_SESSION['errMsg'])){
+		echo $_SESSION['errMsg'];
+	}
+?>
+	<form action='login.php' method='POST'>
+		Email:<input type='email' name='uname'/>
+		Password:<input type='password' name='passwd'/>
+		<input type='submit' value='LogIn'/>
+	</form>
+  <p><a href="createUser.php">Click Here to create a new user</a></p>
+<?php include('assets/includes/footer.html');
 ?>

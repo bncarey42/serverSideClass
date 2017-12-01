78a750aee6eaec0797fb59d58aa6892200023788
bncarey42 <bncarey42@gmail.com>
2017-12-01 00:17:08 -0600

    updates

--- 5/src/profile.php
+++ 5/src/profile.php
@@ -6,45 +6,45 @@
 	}
   $firstName = $_SESSION['fname'];
   $lastName = $_SESSION['lname'];
-echo"<h1>Hello $firstName $lastName</h1>"; ?>
+  echo"<h1>Hello $firstName $lastName</h1>";
+?>
+
 <form action="madLib.php" method="post">
  <fieldset>
    <table>
+
     <?php
-    $getMadLibByID = "SELECT MadLib_ID as mlid, noun_one as noun_one FROM cjohnson_qu5773oo.MadLib WHERE USER_ID = \'$userID\'";
-    require '../SQL_CONNECT.php';
-    $r = @mysqli_query($conn, $getMadLibByID);
-    if($r){
-      $i=0;
-      while($row=mysqli_fetch_array($r, MYSQLI_BOTH)){
-        $rowClass=(($i%2==0) ? "even" : "odd");
-        $mlid = $row['mlid'];
-        $noun=$row['noun_one'];
-          echo"
-            <tr class=\"$rowClass\">
-              <th>
-                <input type=\"radio\" name=\"mlid\" value=\"$mlid\" hidden\>
-                  <input type=\"radio\" />
-                </th>
-              <th>Jack and the $noun</th>
-            </tr>
-          ";
-          $i++;
+      $getMadLibByID = "SELECT MadLib_ID as mlid, noun_one as noun_one FROM cjohnson_qu5773oo.MadLib WHERE USER_ID = \'$userID\'";
+      require '../SQL_CONNECT.php';
+      $r = @mysqli_query($conn, $getMadLibByID);
+      if($r){
+        $i=0;
+        while($row=mysqli_fetch_array($r, MYSQLI_BOTH)){
+          $rowClass=(($i%2==0) ? "even" : "odd");
+          $mlid = $row['mlid'];
+          $noun=$row['noun_one'];
+            echo"
+              <tr class=\"$rowClass\">
+                <th>
+                  <input type=\"radio\" name=\"mlid\" value=\"$mlid\" hidden\>
+                    <input type=\"radio\" />
+                  </th>
+                <th>Jack and the $noun</th>
+              </tr>";
+            $i++;
+          }
+        } else {
+            echo "<tr>
+            <th>No MadLibs found for this user</th>
+            </tr>";
         }
-      } else {
-          echo "<tr>
-          <th>No MadLibs found for this user</th>
-          </tr>";
-      }
-
-
       ?>
+
       <input type="submit" class="button" name="updateMadLib" value="View MadLib" />
       <input type="submit" class="button" name="updateMadLib" value="Delete" />
       <input type="submit" class="button" name="updateMadLib" value="New MadLib" />
-
-
   </table>
 </fieldset>
 </form>
+
 <?php include('assets/includes/footer.php');?>

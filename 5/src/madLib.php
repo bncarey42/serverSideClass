49db6c0a0b8f6aa2daa4b5300271be0502f93437
BenCarey <bncarey42@gmail.com>
2017-12-01 08:55:54 -0600

    Update madLib.php

--- 5/src/madLib.php
+++ 5/src/madLib.php
@@ -1,5 +1,19 @@
-<?php
-  
+<?php 
+  session_start();
+  $action=$_POST['updateMadLib'];
+  $mlid=$_POST['mlid'];  
 
-
+  switch($action){
+    case 'View MadLib':
+      $title = array('m\'lord', 'sir/madam', '');
+      $_SESSION['msg']='Your MadLib, $title[rand(0,4)]';
+      header(Location: );
+      break;
+    case 'Delete':
+      $_SESSION['msg']='Are you sure you want to delete this MadLib?';
+      header(Location: );
+      break;
+    case 'New MadLib':
+      header(Location: );
+      break;
 ?>

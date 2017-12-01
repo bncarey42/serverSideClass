e907568a9331a305876182fdccd438998d90f155
BenCarey <bncarey42@gmail.com>
2017-12-01 10:22:19 -0600

    Update madLib.php

--- 5/src/madLib.php
+++ 5/src/madLib.php
@@ -7,13 +7,14 @@
     case 'View MadLib':
       $title = array('m\'lord', 'sir/madam', 'hommie', 'brother/sister', 'brosef');
       $_SESSION['msg']='Here is your MadLib, $title[rand(0,4)]';
-      header(Location: );
+      header(Location:displayMadLib.php);
       break;
     case 'Delete':
       $_SESSION['msg']='Gaze upon this mad MadLib one last time before it is cast into the void and deleted forever';
-      header(Location: );
+      header(Location:displayMadLib.php);
+      //DELETE FROM cjohnsonqu57733oo.MadLib WHERE Usr_Id=$_SESSION['uid'];
       break;
     case 'New MadLib':
-      header(Location: );
+      header(Location:newMadLib.php);
       break;
 ?>

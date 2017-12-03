70de0aca9cc6d59fc76e7edb050f49989d2e5fe3
bncarey42 <bncarey42@gmail.com>
2017-12-03 13:54:17 -0600

    updates to 5

--- 5/src/assets/includes/displayMadLib.php
+++ 5/src/assets/includes/displayMadLib.php
@@ -1,67 +1,10 @@
 <?php
-$updateMadLib = $_POST['updateMadLib'];
-//set for scope
-$plural_noun_one = '';
-$plural_noun_two = '';
-$adjective_one = '';
-$noun_one = '';
-$adjective_two = '';
-$adjective_three = '';
-$verb = '';
-$body_part = '';
-$adjective_four = '';
-$number = '';
-$noun_two = '';
-
-switch ($updateMadLib) {
-  case 'View MadLib':
-    $mlid = $_POST['mlid'];
-    getMadLibByID($mlid);
-    break;
-  case 'Delete':
-
-    $mlid = $_POST['mlid'];
-    getMadLibByID($mlid);
-
-    $deleteMadLib="DELETE FROM cjohnson_qu5773oo.MadLib WHERE MAdLib_ID=\'$mlid\'";
-    $r = @mysqli_query($conn, $deleteMadLib);
-    if($r) {
-        echo "<h3>While we delete your MadLib here it is one last time before it is deleted forever.</h3>";
-    }else {
-        echo "<h3 class=\"error\">There was an error removing the mad lib from your account please return to your profile and try again</h3>";
-    }
-    break;
-  case 'Submit MadLib':
-    $plural_noun_one = $_POST['plural_noun_one'];
-    $plural_noun_two = $_POST['plural_noun_two'];
-    $adjective_one =   $_POST['adjective_one'];
-    $noun_one =        $_POST['noun_one'];
-    $adjective_two =   $_POST['adjective_two'];
-    $adjective_three = $_POST['adjective_three'];
-    $verb =            $_POST['verb'];
-    $body_part =       $_POST['body_part'];
-    $adjective_four =  $_POST['adjective_four'];
-    $number =          $_POST['number'];
-    $noun_two =        $_POST['noun_two'];
-    $insertMadLib = "INSERT INTO cjohnson_qu5773oo.MadLib(USER_ID, plural_noun_one, plural_noun_two, adjective_one, noun_one, adjective_two, adjective_three,verb,body_part,adjective_four,number,noun_two)
-    VALUES ($currentUID, $plural_noun_one, $plural_noun_two, $adjective_one, $noun_one, $adjective_two, $adjective_three, $verb, $body_part, $adjective_four, $number, $noun_two)";
-    break;
-  default:
-  $plural_noun_one = 'cows';
-  $plural_noun_two = 'coins';
-  $adjective_one =   'old';
-  $noun_one =        'beans';
-  $adjective_two =   'huge';
-  $adjective_three = 'giant';
-  $verb =            'sweat';
-  $body_part =       'blood';
-  $adjective_four =  'angry';
-  $number =          'three';
-  $noun_two =        'eggs';
-    break;
-}
-
-?>
+	include('assets/includes/header.php');
+	if(isset($_SESSION['errMsg'])){
+		echo $_SESSION['errMsg'];
+    $_SESSION['errMsg'] = "";
+	}
+  ?>
 <h2>MadLib</h2>
 <p>
 <h3>Jack and the <?php echo "$noun_one;"; ?></h3>
@@ -74,45 +17,5 @@ switch ($updateMadLib) {
   Englishman.\" The $adjective_four giant chased him down the beanstalk, which Jack cut with an ax. Luckily, jack
   stole the giant's goose that laid $number golden $noun_two for him and his mother.";
   ?>
-  <form action="user.php" method="post">
-    <input type="text" value="$plural_noun_one" hidden="hidden" />
-    <input type="text" value="$plural_noun_two" hidden="hidden" />
-    <input type="text" value="$noun_one" hidden="hidden" />
-    <input type="text" value="$adjective_two" hidden="hidden" />
-    <input type="text" value="$adjective_three" hidden="hidden" />
-    <input type="text" value="$verb" hidden="hidden" />
-    <input type="text" value="$body_part" hidden="hidden" />
-    <input type="text" value="$adjective_four " hidden="hidden" />
-    <input type="submit" class="button" value="Return to Profile"/>
-  </form>
 </p>
-
-
-<?php
-function getMadLibByID($mlid){
-  $getMadLibByID = "SELECT MadLib_ID as mlid, USER_ID as uid, plural_noun_one as plural_noun_one,
-    plural_noun_two as plural_noun_two, adjective_one as adjective_one, noun_one as noun_one,
-    adjective_two as adjective_two, adjective_three as adjective_three, verb as verb,
-    body_part as body_part, adjective_four as adjective_four, number as number, noun_two as noun_two
-    FROM cjohnson_qu5773oo.MadLib WHERE MadLib_ID = \'$mlid\'";
-
-    require('assets/db/mysqli_connect.php');
-    $r=@mysqli_query($conn, $getMadLibByID);
-    if($r){
-      while($row=mysqli_fetch_array($r, MYSQLI_BOTH)){
-          $plural_noun_one = $row['plural_noun_one'];
-          $plural_noun_two = $row['plural_noun_two'];
-          $adjective_one = $row['adjective_one'];
-          $noun_one = $row['noun_one'];
-          $adjective_two = $row['adjective_two'];
-          $adjective_three = $row['adjective_three'];
-          $verb = $row['verb'];
-          $body_part = $row['body_part'];
-          $adjective_four = $row['adjective_four'];
-          $number = $row['number'];
-          $noun_two = $row['noun_two'];
-        }
-        mysqli_close($conn);
-      }
-}
-?>
+<?php include('assets/includes/footer.php');?>
--- 5/src/madLib.php
+++ 5/src/madLib.php
@@ -1,19 +1,54 @@
-<?php 
+<?php
   session_start();
   $action=$_POST['updateMadLib'];
-  $mlid=$_POST['mlid'];  
+  $mlid=$_POST['mlid'];
 
   switch($action){
     case 'View MadLib':
       $title = array('m\'lord', 'sir/madam', 'hommie', 'brother/sister', 'brosef');
       $_SESSION['msg']='Here is your MadLib, $title[rand(0,4)]';
+      require '../SQL_CONNECT';
+      $getMadLibByID = "SELECT MadLib_ID as mlid, USER_ID as uid, plural_noun_one as plural_noun_one,
+        plural_noun_two as plural_noun_two, adjective_one as adjective_one, noun_one as noun_one,
+        adjective_two as adjective_two, adjective_three as adjective_three, verb as verb,
+        body_part as body_part, adjective_four as adjective_four, number as number, noun_two as noun_two
+        FROM cjohnson_qu5773oo.MadLib WHERE MadLib_ID = \'$mlid\'";
+
+        require('assets/db/mysqli_connect.php');
+        $r=@mysqli_query($conn, $getMadLibByID);
+        if($r){
+          while($row=mysqli_fetch_array($r, MYSQLI_BOTH)){
+              $plural_noun_one = $row['plural_noun_one'];
+              $plural_noun_two = $row['plural_noun_two'];
+              $adjective_one = $row['adjective_one'];
+              $noun_one = $row['noun_one'];
+              $adjective_two = $row['adjective_two'];
+              $adjective_three = $row['adjective_three'];
+              $verb = $row['verb'];
+              $body_part = $row['body_part'];
+              $adjective_four = $row['adjective_four'];
+              $number = $row['number'];
+              $noun_two = $row['noun_two'];
+            }
+            mysqli_close($conn);
+          }
+
       header(Location:displayMadLib.php);
       break;
+
     case 'Delete':
-      $_SESSION['msg']='Gaze upon this mad MadLib one last time before it is cast into the void and deleted forever';
+      require '../SQL_CONNECT';
+      $deleteMadLib="DELETE FROM cjohnson_qu5773oo.MadLib WHERE MadLib_ID=\'$mlid\'";
+      $r = @mysqli_query($conn, $deleteMadLib);
+      if($r) {
+          $_SESSION['msg']='Gaze upon this mad MadLib one last time before it is cast into the void and deleted forever';
+      }else {
+          echo "<h3 class=\"error\">There was an error removing the Mad Lib from your account please return to your profile and try again</h3>";
+      }
+
       header(Location:displayMadLib.php);
-      //DELETE FROM cjohnsonqu57733oo.MadLib WHERE Usr_Id=$_SESSION['uid'];
       break;
+
     case 'New MadLib':
       header(Location:newMadLib.php);
       break;

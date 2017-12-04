d88118835d12e5f537ef61f84b5b13c6e5f8ca10
BenCarey <bncarey42@gmail.com>
2017-12-04 17:36:55 -0600

    Update login.php

--- 5/src/login.php
+++ 5/src/login.php
@@ -56,3 +56,53 @@
 			break;
 	}
 ?>
+
+/*
+<?php	session_start();
+	$email=$_POST['email'];
+	$passwd=$_POST['passwd'];
+	$action=$_POST['action'];
+
+	switch ($action) {
+		case 'Log In':
+			logIn('index.php');
+			mysqli_free_result($r);
+			break;
+		case 'Create':
+			$firstName=$_POST['fName'];
+			$lastName=$_POST['lName'];
+			//insert user
+			require '../SQL_CONNECT.php';
+			$sql="INSERT INTO cjohnson_qu5773oo.User(USR_FIRST_NAME, USR_LAST_NAME, USR_EMAIL, USR_PASSWORD) VALUES('$firstName', '$lastName', '$email', '$passwd')";
+			$r=mysqli_query($conn, $sql);
+			if($r){
+				logIn('newUser.php');
+			}
+			break;
+		default:
+			$_SESSION['errMsg'] = "<p class=\'err\'>Error logging in</p>";
+			header('Location:index.php');
+			break;
+	}
+	mysql_close($conn);
+	
+	
+	function logIn($originPage){
+		//select user from db
+		require '../SQL_CONNECT.php';
+		$sql="SELECT USR_ID as uid, USR_FIRST_NAME as fn, USR_LAST_NAME as ln FROM cjohnson_qu5773oo.User WHERE USR_EMAIL='$email' AND USR_PASSWORD='$passwd'";
+		$r=mysqli_query($conn, $sql);
+		if($r){
+			$row=mysqli_fetch_assoc($r);
+			$_SESSION['uid'] = $row['uid']+1;
+			$fname = $row['fn'];
+			$lname = $row['ln'];
+			$_SESSION['uname'] = $fname." ".$lname;
+			header('Location:profile.php');
+		} else {
+			$_SESSION['errMsg'] = "<p class=\'err\'>Error creating user</p>";
+			header('Location:$originPage');
+		}
+		mysqli_free_result($r);
+?>
+*/

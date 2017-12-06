c3869acff4f057f3ec95921ded46372c40d86f14
BenCarey <bncarey42@gmail.com>
2017-12-06 09:26:00 -0600

    Update header.php

--- project/src/assets/includes/header.php
+++ project/src/assets/includes/header.php
@@ -7,4 +7,12 @@
   <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
 </head>
 <body>
+  <section class="topBar">
+    <table>
+      <tr>
+        <td class="topLeft">PHPLAYER</td>
+        <td class="topRight"><?php if($_SESSION['loggedOn']){echo "<h4>Hello $_SESSION['$uname']! Click Here to <a href='logOff.php'>Log Off</a>.</h4>";} ?> </td>
+      </tr>
+    </table>
+  </section>
   <center>

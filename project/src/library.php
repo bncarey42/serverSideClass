f72d56c3f31bfbad745a8cd070745e04463f168f
BenCarey <bncarey42@gmail.com>
2017-12-06 12:58:40 -0600

    Update library.php

--- project/src/library.php
+++ project/src/library.php
@@ -9,6 +9,7 @@
   <input type='submit' name='libView' class='button' value='Playlists'/>
 </form>
 <?php
+  $viewDetail = array();
   switch($view){
     case 'Albums':
       $title = 'Albums';
@@ -29,4 +30,16 @@
   }
 ?>
   <h2><?php echo"$title"; ?></h2>
+  $i=0;
+  foreach($viewDetail as $id->$name){
+    $rowClass=(($i%2==0) ? "even" : "odd");
+    echo"
+      <tr class='$rowClass'>
+        <td >
+      <input type='checkbox' name='id' value='$id'\>
+        </td>
+      <td>$name</th>
+      </tr>";
+      $i++;
+    }
 <? include('assets/includes/footer.php'); ?>

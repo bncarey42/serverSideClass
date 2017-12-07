92f25c0648226651c0f20142e83c1d948c245de7
BenCarey <bncarey42@gmail.com>
2017-12-07 08:04:53 -0600

    Create legal.php

---
+++ project/legal.php
@@ -0,0 +1,8 @@
+<?php include('assets/includes/header.php'); 
+<p>All Grown Up by Paper Navy is licensed under a Attribution-Noncommercial-Share Alike 3.0 United States License.
+Passages by Podington Bear is licensed under a Attribution-NonCommercial 3.0 International License.
+Piano IV: Cinematic by Podington Bear is licensed under a Attribution-NonCommercial 3.0 International License.
+Backbeat by Podington Bear is licensed under a Attribution-NonCommercial 3.0 International License. 
+Strangers With Shoes by Robin Grey is licensed under a Attribution-Noncommercial-Share Alike 3.0 United States License.
+Only The Missle by Robin Grey is licensed under a Attribution-NonCommercial-NoDerivs 3.0 United States Licencse</p>
+<?php include('assets/includes/footer.php')
